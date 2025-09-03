3e360c75d34741f2959a349adf7b631bc721692d
ygh-sub <yghktihttm+github@gmail.com>
2025-09-03 19:46:02 +0900

    create frontend and backend Repositories

---
+++ backend/config/cors.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['http://localhost:3000'],
+];
---
+++ backend/routes/api.php
@@ -0,0 +1,7 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/users', function () {
+    return \App\Models\User::all();
+});
